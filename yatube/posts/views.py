61ca9dff781ff7c58953852d411cb55997a94bc7
Slava Fumin <slavafumin@gmail.com>
2023-02-24 18:53:27 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница приложения post')
+
+
+def group_posts(request):
+    return HttpResponse('Страницы сообщества')
+
+
+def group_posts_name(request, pk):
+    return HttpResponse(f'Страницы сообщества {pk}')
