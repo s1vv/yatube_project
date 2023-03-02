1e5b4ba99ed6dd1e5996903ae68fd18a3b58b602
Slava Fumin <slavafumin@gmail.com>
2023-03-02 08:37:14 +0300

    шаблоны, использование словаря

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,25 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template, {'title': 'Последние обновления на сайте'})
+    text = 'Это главная страница проекта Yatube'
+    title = 'Последние обновления на сайте'
+    context = {
+        'text': text,
+        'title': title
+    }
+    return render(
+        request, template, context 
+    )
+
 
 def group_list(request):
     template = 'posts/group_list.html'
-    return render(request, template, {'title': 'Лев Толстой – зеркало русской революции.'})
+    text = 'Здесь будет информация о группах проекта Yatube'
+    title = 'Лев толстой зеркало русской души'
+    context = {
+        'text': text,
+        'title': title
+    }
+    return render(
+        request, template, context
+    )
